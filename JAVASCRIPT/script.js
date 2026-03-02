66d4f8760a18388e73656a0f392addfa842e1990
ianfranco77 <ian.fd.tic@gmail.com>
2026-03-02 09:51:30 -0600

    Add files via upload

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,14 @@
+//obtener el boton por su ID que se puso 
+const boton1=document.getElementById('tilin');
+//Agregar un evento al boton 
+boton1.addEventListener('click', function(){
+    alert("YEAHH");
+});
+function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "100%";
+    }
+}
